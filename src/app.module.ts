e2f5058b52bc219414fb3fff0138249924c3ceef
NxaSenpai <nxasenpai007@gmail.com>
2026-03-25 15:43:09 +0700

    Done with TP02

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReceiptsModule } from './receipts/receipts.module';
+import { Receipt } from './database/entities/receipts.entities';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'nakry123',
+      database: process.env.DB_NAME || 'tp02_db',
+      entities: [Receipt],
+      synchronize: true,
+      logging: false,
+    }),
+    ReceiptsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+  
+})
+export class AppModule {}
---
+++ src/database/entities/receipts.entities.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('receipts')
+export class Receipt {
+  @PrimaryGeneratedColumn('uuid')
+  receiptId: string;
+
+  @Column({ type: 'timestamp' , default: () => 'CURRENT_TIMESTAMP' })
+  issuedAt: Date;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
